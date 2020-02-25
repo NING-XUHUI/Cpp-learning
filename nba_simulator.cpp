c57277642734f4b95cfc68e96f46ed5a46d6bbef
NING-XUHUI <892897986@qq.com>
2020-02-25 17:52:31 +0800

    cpp

--- nba_simulator.cpp
+++ nba_simulator.cpp
@@ -18,9 +18,8 @@ int RandNo1(){
   return rand()%100;
 }
 */
-
 //延时函数
-void delay_ms(int ms) {
+void delay_ms(unsigned long ms) {
   clock_t start = clock();
   while (clock() - start < ms)
     ;
@@ -112,7 +111,7 @@ int main() {
   Player Durant("杜兰特", 24, 45, 52, 2);
   Player Green("格林", 35, 31, 47, 2);
   Player Looney("鲁尼", 0, 0, 59, 2);
-
+  
   cout << "比赛开始" << endl;
   int sum_time = 2880;
   cout << endl;
