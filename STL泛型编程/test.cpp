e68f5479361c7fb73f930928806ddb98046e4c39
NING-XUHUI <892897986@qq.com>
2020-07-26 22:10:24 +0800

    STL

--- STL泛型编程/test.cpp
+++ STL泛型编程/test.cpp
@@ -6,6 +6,18 @@
 using namespace std;
 
 int main(){
+  /**
+     vector:container
+     alloccator<int>:allocator
+     iterator:vi.begin(),vi.end()
+     algorithm:count_if()
+     function_adapter(negator):not1
+     function_adapter(binder):bind2nd
+     function_object:less<int>
+   */
+
+
+
   int ia[6] = {27,210,12,47,109,83};
   vector<int,allocator<int>> vi(ia,ia+6);
   cout << count_if(vi.begin(),vi.end(),not1(bind2nd(less<int>(),10)));
